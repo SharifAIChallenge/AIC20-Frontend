b1630bafa6e24f3218bd1f1249dfe2af377ddd78
Hamed <amzhamed@gmail.com>
2020-02-04 15:22:41 +0330

    Fix sponsor tile size

--- components/landing/Organizers.vue
+++ components/landing/Organizers.vue
@@ -1,38 +1,46 @@
 <template>
   <v-container class="py-12">
-    <glow class="mb-8">
-      <h2 id="organizer" class="display-2 transparent--text text-stroke-primary text-stroke-width-2 font-weight-black">
-        {{
-        $t("home.organizer") }}</h2>
-    </glow>
-    <v-row justify="center">
-      <v-col  cols="8" sm="6" md="5" lg="4">
-        <v-card color="white" class="pa-5">
-          <img src="/image/SSC.png" style="max-width: 100%; border-radius: 0!important;"/>
-        </v-card>
+    <v-row>
+      <v-col cols="12" md="6">
+        <glow class="mb-8">
+          <h2 id="organizer" class="display-2 transparent--text text-stroke-primary text-stroke-width-2 font-weight-black">
+            {{
+            $t("home.organizer") }}</h2>
+        </glow>
+        <v-row class="justify-center justify-md-start">
+          <v-col  cols="8" sm="6" md="5" lg="4">
+            <v-card color="white" class="pa-5">
+              <img src="/image/SSC.png" style="max-width: 100%; border-radius: 0!important;"/>
+            </v-card>
+          </v-col>
+        </v-row>
       </v-col>
-    </v-row>
-    <glow class="my-8">
-      <h2 class="display-2 transparent--text text-stroke-primary text-stroke-width-2 font-weight-black">{{
-        $t("home.sponsors") }}</h2>
-    </glow>
-    <v-row align="stretch" justify="center">
-      <v-col cols="5" sm="4" md="3" lg="3">
-        <v-card color="white" class="pa-5">
-          <img src="/image/Alibaba.png" style="max-width: 100%; border-radius: 0!important;"/>
-        </v-card>
-      </v-col>
-      <v-col cols="5" sm="4" md="3" lg="3">
-        <v-card color="white" class="pa-5 fill-height d-flex align-center">
-          <img src="/image/Tapsi.png" style="max-width: 100%; border-radius: 0!important;"/>
-        </v-card>
-      </v-col>
-    </v-row>
-    <v-row justify="center">
-      <v-col cols="4" sm="3" md="2" lg="2">
-        <v-card color="white" class="pa-5 fill-height d-flex align-center">
-          <img src="/image/Aparat.png" style="max-width: 100%; border-radius: 0!important;"/>
-        </v-card>
+      <v-col cols="12" md="6">
+        <glow class="mb-8">
+          <h2 class="display-2 transparent--text text-stroke-primary text-stroke-width-2 font-weight-black">{{
+            $t("home.sponsors") }}</h2>
+        </glow>
+        <v-row align="stretch" class="justify-center justify-md-start">
+<!--              <v-img src="/image/Alibaba.png" max-height="100" contain/>-->
+<!--              <v-img src="/image/Tapsi.png" max-height="100" contain/>-->
+          <v-col cols="4" sm="3" md="3">
+            <v-card color="white" class="pa-1">
+              <img src="/image/Alibaba.png" style="max-width: 100%; border-radius: 0!important;"/>
+            </v-card>
+          </v-col>
+          <v-col cols="4" sm="3" md="3">
+            <v-card color="white" class="pa-1 fill-height d-flex align-center">
+              <img src="/image/Tapsi.png" style="max-width: 100%; border-radius: 0!important;"/>
+            </v-card>
+          </v-col>
+        </v-row>
+        <v-row class="justify-center justify-md-start">
+          <v-col cols="3" sm="2" md="2">
+            <v-card color="white" class="pa-3 fill-height d-flex align-center">
+              <img src="/image/Aparat.png" style="max-width: 100%; border-radius: 0!important;"/>
+            </v-card>
+          </v-col>
+        </v-row>
       </v-col>
     </v-row>
   </v-container>
