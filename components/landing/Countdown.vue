72c59aff4a94e22c12091ad66c7ea2b31c8cd6a3
Hamed <amzhamed@gmail.com>
2020-02-04 16:22:28 +0330

    Fix layout on mobile

--- components/landing/Countdown.vue
+++ components/landing/Countdown.vue
@@ -1,18 +1,20 @@
 <template>
-  <div class="d-flex flex-column justify-center countdown" style="height: 400px;">
-    <div>
-      <countdown-timer :deadline="new Date(2020,1,15, 21).getTime()"/>
-    </div>
-    <div class="mt-5">
-      <v-row class="justify-center">
-        <glow>
-          <v-col class="text-center display-1">
-            {{$t("home.tillCompetitionStart") }}
-          </v-col>
-        </glow>
-      </v-row>
-    </div>
-  </div>
+  <v-container>
+    <v-row class="flex-column justify-center py-12">
+      <v-col>
+        <countdown-timer :deadline="new Date(2020,1,15, 21).getTime()"/>
+      </v-col>
+      <v-col class="mt-5">
+        <v-row class="justify-center">
+          <glow>
+            <v-col class="text-center display-1">
+              {{$t("home.tillCompetitionStart") }}
+            </v-col>
+          </glow>
+        </v-row>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script>
