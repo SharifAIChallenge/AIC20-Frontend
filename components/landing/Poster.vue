f5f17a4ec5dc9fed3d92b36c1866a789ae068edc
Hamed <amzhamed@gmail.com>
2020-02-05 01:01:35 +0330

    Add teaser

--- components/landing/Poster.vue
+++ components/landing/Poster.vue
@@ -7,31 +7,74 @@
       <glow>
         <v-col class="text-center" cols="12">
           <div>
-            <span class="display-1 display-xs-2 font-family-chopsic text-stroke-width-2 text-stroke-white transparent--text">sharif</span>
-            <span class="display-2 display-xs-4 font-family-chopsic pl-3 text-stroke-width-2 text-stroke-primary transparent--text">AI</span>
+            <span
+              class="display-1 display-xs-2 font-family-chopsic text-stroke-width-2 text-stroke-white transparent--text">sharif</span>
+            <span
+              class="display-2 display-xs-4 font-family-chopsic pl-3 text-stroke-width-2 text-stroke-primary transparent--text">AI</span>
           </div>
-          <div class="display-1 display-xs-2 font-family-chopsic text-stroke-width-2 text-stroke-white transparent--text">Challenge
+          <div
+            class="display-1 display-xs-2 font-family-chopsic text-stroke-width-2 text-stroke-white transparent--text">
+            Challenge
           </div>
-          <div class="display-2 display-xs-4 font-family-chopsic text-stroke-width-2 text-stroke-primary transparent--text">2020
+          <div
+            class="display-2 display-xs-4 font-family-chopsic text-stroke-width-2 text-stroke-primary transparent--text">
+            2020
           </div>
         </v-col>
       </glow>
     </v-row>
     <v-row v-if="!$auth.loggedIn" class="justify-center">
-      <v-btn rounded x-large light color="primary" to="/signup">
-        <v-icon left>mdi-rocket</v-icon>
-        {{ $t("form.registerNow") }}
-      </v-btn>
+      <div>
+        <v-btn rounded x-large light color="primary" to="/signup" block>
+          <v-icon left>mdi-rocket</v-icon>
+          {{ $t("form.registerNow") }}
+        </v-btn>
+        <v-dialog v-model="dialog" width="900">
+          <template v-slot:activator="{ on }">
+            <v-btn
+              class="mt-5"
+              rounded
+              outlined
+              v-on="on"
+              block
+              x-large
+            >
+              <v-icon color="primary" left>mdi-play</v-icon>
+              {{ $t("home.watchTeaser") }}
+            </v-btn>
+          </template>
+
+          <v-card>
+            <v-btn icon large class="p-absolute" style="z-index: 1" @click="dialog=false">
+              <v-icon>mdi-close</v-icon>
+            </v-btn>
+            <v-btn loading text color="accent" class="p-absolute" block style="top: 50%;"/>
+            <v-card-text class="pa-0" dir="ltr">
+              <v-responsive :aspect-ratio="16/9">
+                <keep-alive>
+                  <teaser v-if="dialog"/>
+                </keep-alive>
+              </v-responsive>
+            </v-card-text>
+          </v-card>
+        </v-dialog>
+      </div>
     </v-row>
   </v-parallax>
 </template>
 
 <script>
   import Glow from "../Glow";
+  import Teaser from "./Teaser";
 
   export default {
     name: "Poster",
-    components: { Glow },
+    components: { Teaser, Glow },
+    data() {
+      return {
+        dialog: false
+      };
+    },
     computed: {
       size() {
         return this.$vuetify.breakpoint.xsOnly ? 1 : 2;
@@ -46,6 +89,7 @@
       font-size: 6rem !important;
       line-height: 6rem;
     }
+
     .v-application .display-xs-2 {
       font-size: 3rem !important;
       line-height: 3.125rem;
---
+++ components/landing/Teaser.vue
@@ -0,0 +1,18 @@
+<template>
+  <client-only>
+    <div id="464529485" class="player">
+      <script type="text/JavaScript"
+              src="https://www.aparat.com/embed/eLV7H?data[rnddiv]=464529485&data[responsive]=yes"></script>
+    </div>
+  </client-only>
+</template>
+
+<script>
+  export default {
+    name: "Teaser"
+  };
+</script>
+
+<style scoped>
+
+</style>
