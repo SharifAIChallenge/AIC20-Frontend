6dd4610ad2f75aa75e1e04c9629cc79565940145
Hamed <amzhamed@gmail.com>
2020-02-06 19:40:36 +0330

    Add dashboard home and settings

---
+++ pages/dashboard/index.vue
@@ -0,0 +1,13 @@
+<template>
+
+</template>
+
+<script>
+  export default {
+    layout: "dashboard"
+  };
+</script>
+
+<style scoped>
+
+</style>
---
+++ pages/dashboard/settings.vue
@@ -0,0 +1,51 @@
+<template>
+  <v-row class="flex-column align-center">
+    <v-col>
+      <h2 class="ps-3">{{ $t("dashboard.settings") }}</h2>
+    </v-col>
+    <v-col>
+      <v-card class="overflow-hidden">
+        <v-tabs icons-and-text :grow="mobile" :vertical="!mobile">
+          <v-tab>
+            {{ $t("dashboard.editProfile") }}
+            <v-icon>mdi-account-edit</v-icon>
+          </v-tab>
+          <v-tab>
+            {{ $t("form.changePassword") }}
+            <v-icon>mdi-shield-edit</v-icon>
+          </v-tab>
+
+          <v-tab-item>
+            <v-card-text class="mt-3">
+              <edit-profile/>
+            </v-card-text>
+          </v-tab-item>
+          <v-tab-item>
+            <v-card-text class="mt-3">
+              <change-password/>
+            </v-card-text>
+          </v-tab-item>
+        </v-tabs>
+      </v-card>
+    </v-col>
+  </v-row>
+</template>
+
+<script>
+  import EditProfile from "../../components/dashboard/EditProfile";
+  import ChangePassword from "../../components/dashboard/ChangePassword";
+
+  export default {
+    components: { ChangePassword, EditProfile },
+    layout: "dashboard",
+    computed: {
+      mobile() {
+        return this.$vuetify.breakpoint.xsOnly;
+      }
+    }
+  };
+</script>
+
+<style scoped>
+
+</style>
