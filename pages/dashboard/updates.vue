66a4c7148e0d044de6ef06077e770ab8d3ba6083
Hamed <amzhamed@gmail.com>
2020-02-08 06:48:58 +0330

    Init dashboard pages

---
+++ pages/dashboard/updates.vue
@@ -0,0 +1,20 @@
+<template>
+  <dashboard-page title="dashboard.updates">
+    <v-card>
+      <updates/>
+    </v-card>
+  </dashboard-page>
+</template>
+
+<script>
+  import Updates from "../../components/dashboard/Updates";
+  import DashboardPage from "../../components/dashboard/DashboardPage";
+  export default {
+    layout: "dashboard",
+    components: { DashboardPage, Updates }
+  };
+</script>
+
+<style scoped>
+
+</style>
