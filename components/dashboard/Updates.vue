9471fecf866fa0443e7cb1408171ff7e679e42d8
Hamed <amzhamed@gmail.com>
2020-02-08 06:47:19 +0330

    Add Updates component

---
+++ components/dashboard/Updates.vue
@@ -0,0 +1,30 @@
+<template>
+  <v-timeline dense>
+    <v-slide-x-reverse-transition group hide-on-leave>
+      <v-timeline-item
+        v-for="item in items"
+        :key="item.id"
+        :color="item.title"
+        small
+      >
+        <v-alert
+          text
+          :color="item.title"
+          class="my-0 white--text"
+        >
+          {{ item.text }}
+        </v-alert>
+      </v-timeline-item>
+    </v-slide-x-reverse-transition>
+  </v-timeline>
+</template>
+
+<script>
+  export default {
+    props: ["items"]
+  };
+</script>
+
+<style scoped>
+
+</style>
