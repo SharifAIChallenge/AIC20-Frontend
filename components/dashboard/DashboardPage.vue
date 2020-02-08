ec59a9e0db51aaee955219843fd2b4ef01158daf
Hamed <amzhamed@gmail.com>
2020-02-08 06:45:35 +0330

    Add dashboard page

---
+++ components/dashboard/DashboardPage.vue
@@ -0,0 +1,26 @@
+<template>
+  <v-row class="flex-column align-center">
+    <v-col class="pa-0 mt-md-n11" :class="{'hidden-md-and-up': !permanent}">
+      <v-col class="pt-md-0 mt-md-n5 pb-md-7">
+        <h2 class="ps-3">{{ $t(title) }}</h2>
+      </v-col>
+    </v-col>
+    <v-col>
+      <slot/>
+    </v-col>
+  </v-row>
+</template>
+
+<script>
+  export default {
+    props: {
+      title: {
+        type: String
+      },
+      permanent: {
+        type: Boolean,
+        default: false
+      }
+    }
+  };
+</script>
