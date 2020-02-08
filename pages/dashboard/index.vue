2031c9bbb6886d2e80d4457f7df5b73d7bb7f406
Hamed <amzhamed@gmail.com>
2020-02-08 06:45:13 +0330

    Add updates, performance, and stats to home

--- pages/dashboard/index.vue
+++ pages/dashboard/index.vue
@@ -1,10 +1,69 @@
 <template>
-
+  <v-row>
+    <v-col cols="12">
+      <v-card>
+        <v-card-title>
+          {{ $t("dashboard.recentUpdates") }}
+          <v-spacer/>
+          <v-btn rounded small color="secondary" to="/dashboard/updates" nuxt>{{ $t("dashboard.seeAll") }}</v-btn>
+        </v-card-title>
+        <v-divider/>
+        <v-card-text>
+          <updates :items="updates"/>
+        </v-card-text>
+        <v-card-actions>
+        </v-card-actions>
+      </v-card>
+    </v-col>
+    <v-col cols="12" md="6">
+      <v-card>
+        <v-card-title>{{ $t("dashboard.gameStats") }}</v-card-title>
+        <v-divider/>
+        <v-card-text class="text-center">
+          <games-stat :wins="gameStat.wins" :loss="gameStat.loss"/>
+        </v-card-text>
+      </v-card>
+    </v-col>
+    <v-col cols="12" md="6">
+      <v-card>
+        <v-card-title>{{ $t("dashboard.performance") }}</v-card-title>
+        <v-divider/>
+        <v-card-text>
+          <performance/>
+        </v-card-text>
+      </v-card>
+    </v-col>
+  </v-row>
 </template>
 
 <script>
+  import Updates from "../../components/dashboard/Updates";
+  import GamesStat from "../../components/dashboard/GamesStat";
+  import Performance from "../../components/dashboard/Performance";
+
   export default {
-    layout: "dashboard"
+    components: { Performance, GamesStat, Updates },
+    layout: "dashboard",
+    data() {
+      return {
+        updates: [
+          {
+            id: 1,
+            text: "salam",
+            title: "info"
+          },
+          {
+            id: 2,
+            text: "salam",
+            title: "warning"
+          }
+        ],
+        gameStat: {
+          wins: 2,
+          loss: 1
+        }
+      };
+    }
   };
 </script>
 
