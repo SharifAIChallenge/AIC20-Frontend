a30a83ed7a04037ada450411bc971a1f4a3b189d
Hamed <amzhamed@gmail.com>
2020-02-09 01:23:32 +0330

    Add tutorials

--- pages/dashboard/tutorials.vue
+++ pages/dashboard/tutorials.vue
@@ -1,10 +1,21 @@
 <template>
-
+  <dashboard-page title="dashboard.tutorials">
+    <resource-page :data="data"/>
+  </dashboard-page>
 </template>
 
 <script>
+  import DashboardPage from "../../components/dashboard/DashboardPage";
+  import dashboardPageValidate from "../../mixins/dashboardPageValidate";
+  import ResourcePage from "../../components/dashboard/ResourcePage";
+
   export default {
+    components: { ResourcePage, DashboardPage },
     layout: "dashboard",
+    mixins: [dashboardPageValidate("tutorials")],
+    async asyncData({ $axios }) {
+      return $axios.get("/resources/1");
+    }
   };
 </script>
 
