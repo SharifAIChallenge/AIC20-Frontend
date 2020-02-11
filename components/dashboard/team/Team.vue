a674c13eb31f8605e83bd0cdcee41a5513e19b19
Hamed <amzhamed@gmail.com>
2020-02-11 19:25:34 +0330

    Fix style and remove leave team btn

--- components/dashboard/team/Team.vue
+++ components/dashboard/team/Team.vue
@@ -1,50 +1,40 @@
 <template>
   <div>
     <div class="text-center my-3">
-      <v-avatar size="150" :color="colors[team.name.length % colors.length]">
-        <v-img v-if="team.image" :src="team.image"/>
-        <span v-else class="text-uppercase display-3 pt-4 font-weight-bold">{{ team.name[0] }}</span>
-      </v-avatar>
+      <v-badge
+        bordered
+        :color="team.is_valid ? 'success' : 'error'"
+        :icon="team.is_valid ? 'mdi-check-decagram' : 'mdi-lock'"
+        overlap
+        offset-x="30"
+        offset-y="30"
+      >
+        <v-avatar size="150" :color="colors[team.name.length % colors.length]">
+          <v-img v-if="team.image" :src="team.image"/>
+          <span v-else class="text-uppercase display-3 pt-4 font-weight-bold">{{ team.name[0] }}</span>
+        </v-avatar>
+      </v-badge>
       <div class="mt-4 title">{{ team.name }}</div>
     </div>
-    <v-list class="mb-4">
-      <v-list-item v-for="item in items" :key="item.title">
-        <v-list-item-avatar>
-          <v-img :src="item.avatar"/>
+    <v-list>
+      <v-list-item v-for="item in team.participants" :key="item.user.email">
+        <v-list-item-avatar
+          :color="color(item)"
+          class="pt-1 text-uppercase justify-center">
+          {{ item.user.profile.firstname_en[0] + item.user.profile.lastname_en[0] }}
         </v-list-item-avatar>
 
         <v-list-item-content>
-          <v-list-item-title v-text="item.title"/>
+          <v-list-item-title v-text="item.user.profile.firstname_fa + ' ' + item.user.profile.lastname_fa"/>
         </v-list-item-content>
       </v-list-item>
     </v-list>
-
-    <v-dialog v-model="dialog" max-width="320">
-      <template v-slot:activator="{ on }">
-        <v-btn large color="white" rounded outlined dark v-on="on">
-          <v-icon color="error" left>mdi-exit-run</v-icon>
-          {{ $t("dashboard.leaveTeam") }}
-        </v-btn>
-      </template>
-      <v-card>
-        <v-card-title style="word-break: unset">
-          {{ leaveMessage }}
-        </v-card-title>
-        <v-card-actions>
-          <v-col>
-            <v-btn block rounded color="primary" @click="dialog = false">{{ $t("form.cancel") }}</v-btn>
-          </v-col>
-          <v-col>
-            <v-btn block rounded outlined @click="dialog = false">{{ $t("dashboard.leaveTeam") }}</v-btn>
-          </v-col>
-        </v-card-actions>
-      </v-card>
-    </v-dialog>
   </div>
 </template>
 
 <script>
   import { mapState } from "vuex";
+  import { LEAVE_TEAM } from "../../../api";
 
   export default {
     props: {
@@ -52,21 +42,15 @@
         type: Object
       }
     },
-    data() {
-      return {
-        dialog: false,
-        leaveMessage: "می‌خواهید تیمتان را ترک کنید؟",
-        items: [
-          // { title: 'Jason Oner', avatar: 'https://cdn.vuetifyjs.com/images/lists/1.jpg' },
-          // { title: 'Ranee Carlson', avatar: 'https://cdn.vuetifyjs.com/images/lists/2.jpg' },
-          // { title: 'Cindy Baker', avatar: 'https://cdn.vuetifyjs.com/images/lists/3.jpg' },
-        ],
-      };
-    },
     computed: {
       ...mapState({
         colors: state => state.dashboard.colors
       })
+    },
+    methods: {
+      color(item) {
+        return this.colors[(Object.values(item.user.profile).join().length + 1 + item.user.email.length) % this.colors.length];
+      }
     }
   };
 </script>
