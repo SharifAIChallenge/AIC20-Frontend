d5ebcd535bc929b55a18cefe60ef4569ffc864c8
Hamed <amzhamed@gmail.com>
2020-02-11 19:23:42 +0330

    Add toast

--- components/dashboard/team/CreateTeam.vue
+++ components/dashboard/team/CreateTeam.vue
@@ -1,6 +1,8 @@
 <template>
   <v-form ref="createTeam" v-model="valid" @submit="createTeam" onSubmit="return false;">
-
+    <v-alert text icon="mdi-information" class="mb-6">
+      {{ $t("dashboard.createTeamMessage") }}
+    </v-alert>
     <v-text-field
       v-model="name"
       :label="$t('form.teamName')"
@@ -17,10 +19,9 @@
       :rules="imageRules"
       :hint="imageHint"
       show-size
+      persistent-hint
+      class="mb-3"
     />
-    <v-alert :type="result.type" v-model="result.value" text outlined dismissible>
-      {{ result.message }}
-    </v-alert>
 
     <v-btn :disabled="!valid" :loading="loading" type="submit" v-bind="primaryButtonProps">
       <v-icon left>mdi-plus-circle-outline</v-icon>
@@ -47,18 +48,13 @@
           value => !value || !value.size || value.size < 200000 || "سایز عکس باید کمتر از ۲۰۰ کیلوبایت باشد."
         ],
         imageHint: "عکس مربع با حجم حداکثر ۲۰۰ کیلوبایت",
-        result: {
-          value: false,
-          type: "success",
-          message: ""
-        },
         loading: false
       };
     },
     methods: {
       async createTeam() {
         const formData = new FormData();
-        formData.append("image", this.image);
+        if (this.image) formData.append("image", this.image);
         formData.append("name", this.name);
         const config = {
           url: CREATE_TEAM.url,
@@ -68,19 +64,15 @@
         this.loading = true;
         let { data } = await this.$axios(config);
         this.loading = false;
+        this.$store.dispatch("team/getTeam");
         if (data.status_code) {
           if (data.status_code === 200) {
-            this.result.message = "تیم با موفقیت ساخته شد.";
-            this.result.type = "success";
-            this.result.value = true;
+            this.$toast.success("تیم با موفقیت ساخته شد.");
             this.$refs.createTeam.reset();
           } else {
-            this.result.message = "خطایی در ساخت تیم رخ داد.";
-            this.result.type = "error";
-            this.result.value = true;
+            this.$toast.error("خطایی در ساخت تیم رخ داد.");
           }
         }
-
       }
     }
   };
