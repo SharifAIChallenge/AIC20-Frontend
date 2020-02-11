bf6a8ff2acec99ff309017acba7012607a633a77
Hamed <amzhamed@gmail.com>
2020-02-11 19:26:14 +0330

    Add invitation status icon

---
+++ components/dashboard/StatusIcon.vue
@@ -0,0 +1,27 @@
+<template>
+  <v-icon :color="icon[status].color">{{ icon[status].i }}</v-icon>
+</template>
+
+<script>
+  export default {
+    props: {
+      status: {
+        type: String,
+        default: "not_answered"
+      }
+    },
+    data() {
+      return {
+        icon: {
+          accepted: { i: "mdi-checkbox-marked-circle-outline", color: "success" },
+          not_answered: { i: "mdi-clock-outline", color: "info" },
+          rejected: { i: "mdi-close-circle-outline", color: "error" }
+        }
+      };
+    }
+  };
+</script>
+
+<style scoped>
+
+</style>
