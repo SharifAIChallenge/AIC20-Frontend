6f274bff6f426df8e6f25e5a90b14b9398dc0d8d
Hamed <amzhamed@gmail.com>
2020-02-13 18:52:52 +0330

    Add submissions pge

--- pages/dashboard/submissions.vue
+++ pages/dashboard/submissions.vue
@@ -1,13 +1,45 @@
 <template>
-
+  <dashboard-page title="dashboard.submissions">
+    <v-col>
+      <v-card>
+        <v-card-text>
+          <code-submission/>
+        </v-card-text>
+      </v-card>
+    </v-col>
+    <v-col>
+      <v-card>
+        <v-card-title>{{ $t("dashboard.submissionsHistory") }}
+          <v-btn icon class="ms-3" @click="$store.dispatch('team/getSubmissions')">
+            <v-icon>mdi-autorenew</v-icon>
+          </v-btn>
+        </v-card-title>
+        <v-divider/>
+        <submissions-list :submissions="submissions"/>
+      </v-card>
+    </v-col>
+  </dashboard-page>
 </template>
 
 <script>
   import dashboardPageValidate from "../../mixins/dashboardPageValidate";
+  import DashboardPage from "../../components/dashboard/DashboardPage";
+  import CodeSubmission from "../../components/dashboard/submission/CodeSubmission";
+  import SubmissionsList from "../../components/dashboard/submission/SubmissionsList";
+  import { mapState } from "vuex";
 
   export default {
+    components: { CodeSubmission, DashboardPage, SubmissionsList },
     layout: "dashboard",
-    mixins: [dashboardPageValidate("submissions")]
+    mixins: [dashboardPageValidate("submissions")],
+    async fetch({ store }) {
+      await store.dispatch("team/getSubmissions")
+    },
+    computed: {
+      ...mapState({
+        submissions: state => state.team.submissions,
+      })
+    }
   };
 </script>
 
