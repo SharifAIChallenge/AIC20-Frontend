df8b97530225e8c7119eb66ff411892e4e803864
Hamed <amzhamed@gmail.com>
2020-02-15 14:20:24 +0330

    Fix team loader

--- components/dashboard/team/TeamLoader.vue
+++ components/dashboard/team/TeamLoader.vue
@@ -2,23 +2,39 @@
   <div>
     <v-skeleton-loader
       v-if="loading"
-      height="250"
-      type=" image, list-item-avatar"
+      :height="50*count"
+      :type="`list-item@${count}`"
     >
     </v-skeleton-loader>
-    <team v-else-if="team" :team="team"/>
+    <v-list v-else-if="team">
+      <div v-if="avatar" class="text-center">
+        <team-avatar :team="team" :size="64"/>
+      </div>
+      <v-list-item v-for="(item, i) in team.participants" :key="item+i">
+        <v-list-item-content>{{ item.user.profile.firstname_fa + " " + item.user.profile.lastname_fa }}
+        </v-list-item-content>
+      </v-list-item>
+    </v-list>
   </div>
 </template>
 
 <script>
   import Team from "./Team";
   import { TEAM_DETAIL } from "../../../api";
+  import TeamAvatar from "./TeamAvatar";
 
   export default {
-    components: { Team },
+    components: { TeamAvatar, Team },
     props: {
       name: {
         type: String
+      },
+      avatar: {
+        type: Boolean
+      },
+      count : {
+        type: Number,
+        default: 2
       }
     },
     data() {
