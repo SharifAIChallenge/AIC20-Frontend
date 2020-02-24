c0c75a4d2f7561b8bb85aac5acfda66710f0583b
Hamed <amzhamed@gmail.com>
2020-02-24 17:33:51 +0330

    Remove sponsors

--- components/landing/Organizers.vue
+++ components/landing/Organizers.vue
@@ -19,39 +19,39 @@
     </div>
     <v-divider class="my-12"/>
     <v-row>
-      <v-col cols="12" md="6">
+      <v-col>
         <glow class="mb-8">
           <h2 id="organizer"
               class="display-2 transparent--text text-stroke-primary text-stroke-width-2 font-weight-black">
             {{
             $t("home.organizer") }}</h2>
         </glow>
-        <v-row class="justify-center justify-md-start">
-          <v-col cols="8" sm="6" md="5" lg="4">
+        <v-row class="justify-center">
+          <v-col cols="6" sm="5" md="4" lg="2">
             <v-card color="white" class="pa-5" href="https://ssc.ce.sharif.edu/" target="_blank">
               <img src="/image/SSC.png" style="max-width: 100%; border-radius: 0!important;"/>
             </v-card>
           </v-col>
         </v-row>
       </v-col>
-      <v-col cols="12" md="6">
-        <glow class="mb-8">
-          <h2 class="display-2 transparent--text text-stroke-primary text-stroke-width-2 font-weight-black">{{
-            $t("home.sponsors") }}</h2>
-        </glow>
-        <v-row align="stretch" class="justify-center justify-md-start">
-          <v-col cols="4" sm="3" md="3">
-            <v-card color="white" class="pa-1">
-              <img src="/image/Alibaba.png" style="max-width: 100%; border-radius: 0!important;"/>
-            </v-card>
-          </v-col>
-          <v-col cols="4" sm="3" md="3">
-            <v-card color="white" class="pa-1 fill-height d-flex align-center">
-              <img src="/image/Tapsi.png" style="max-width: 100%; border-radius: 0!important;"/>
-            </v-card>
-          </v-col>
-        </v-row>
-      </v-col>
+<!--      <v-col cols="12" md="6">-->
+<!--        <glow class="mb-8">-->
+<!--          <h2 class="display-2 transparent&#45;&#45;text text-stroke-primary text-stroke-width-2 font-weight-black">{{-->
+<!--            $t("home.sponsors") }}</h2>-->
+<!--        </glow>-->
+<!--        <v-row align="stretch" class="justify-center justify-md-start">-->
+<!--          <v-col cols="4" sm="3" md="3">-->
+<!--            <v-card color="white" class="pa-1">-->
+<!--              <img src="/image/Alibaba.png" style="max-width: 100%; border-radius: 0!important;"/>-->
+<!--            </v-card>-->
+<!--          </v-col>-->
+<!--          <v-col cols="4" sm="3" md="3">-->
+<!--            <v-card color="white" class="pa-1 fill-height d-flex align-center">-->
+<!--              <img src="/image/Tapsi.png" style="max-width: 100%; border-radius: 0!important;"/>-->
+<!--            </v-card>-->
+<!--          </v-col>-->
+<!--        </v-row>-->
+<!--      </v-col>-->
     </v-row>
   </v-container>
 </template>
