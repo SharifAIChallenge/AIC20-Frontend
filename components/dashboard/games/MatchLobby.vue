230d44078504379df1d0fc7aecc83bbbbef038c8
Hamed <amzhamed@gmail.com>
2020-02-27 07:43:55 +0330

    Add lobby item

--- components/dashboard/games/MatchLobby.vue
+++ components/dashboard/games/MatchLobby.vue
@@ -1,41 +1,24 @@
 <template>
   <div>
-    <v-row justify="center">
-      <v-col>
-        <div class="d-flex justify-space-around">
-          <v-col v-for="i in 2" :key="i" cols="12" sm="6" class="d-flex justify-center">
-            <v-chip pill v-if="i-1 < lobby.teams1.length">
-              <team-avatar left :team="{name: lobby.teams1[i-1].name}"/>
-              {{ lobby.teams1[i-1].name }}
-            </v-chip>
-            <v-skeleton-loader v-else type="chip"/>
-          </v-col>
-        </div>
-      </v-col>
-      <v-col class="text-center pa-6" cols="1">
-        <v-icon>mdi-sword-cross</v-icon>
-      </v-col>
-      <v-col>
-        <div class="d-flex justify-space-around">
-          <v-col v-for="i in 2" :key="i" cols="12" sm="6" class="d-flex justify-center">
-            <v-chip pill v-if="i-1 < lobby.teams2.length">
-              <team-avatar left :team="{name: lobby.teams2[i-1].name}"/>
-              {{ lobby.teams2[i-1].name }}
-            </v-chip>
-            <v-skeleton-loader v-else type="chip"/>
-          </v-col>
-        </div>
-      </v-col>
-    </v-row>
+    <v-slide-y-transition group>
+      <match-lobby-item v-for="(lobby, i) in lobbies" :lobby="lobby" :key="i"/>
+    </v-slide-y-transition>
+    <p v-if="!lobbies.length" class="text-center">{{ $t("dashboard.noLobbies") }}</p>
   </div>
 </template>
 
 <script>
-  import TeamAvatar from "../team/TeamAvatar";
+  import MatchLobbyItem from "./MatchLobbyItem";
+  import { mapState } from "vuex";
 
   export default {
-    components: { TeamAvatar },
-    props: ["lobby"]
+    name: "MatchLobby",
+    components: { MatchLobbyItem },
+    computed: {
+      ...mapState({
+        lobbies: state => state.games.friendlyLobbies
+      })
+    }
   };
 </script>
 
---
+++ components/dashboard/games/MatchLobbyItem.vue
@@ -0,0 +1,44 @@
+<template>
+  <div>
+    <v-row justify="center">
+      <v-col>
+        <v-row>
+          <v-col v-for="i in 2" :key="i" cols="12" sm="6" class="d-flex justify-center px-0">
+            <v-chip pill v-if="i-1 < lobby.teams1.length">
+              <team-avatar left :team="{name: lobby.teams1[i-1].name, image: lobby.teams1[i-1].image}"/>
+              {{ lobby.teams1[i-1].name }}
+            </v-chip>
+            <v-skeleton-loader v-else type="chip"/>
+          </v-col>
+        </v-row>
+      </v-col>
+      <v-col cols="1" class="d-flex align-center justify-center">
+        <v-icon>mdi-sword-cross</v-icon>
+      </v-col>
+      <v-col>
+        <v-row>
+          <v-col v-for="i in 2" :key="i" cols="12" sm="6" class="d-flex justify-center px-0">
+            <v-chip pill v-if="i-1 < lobby.teams2.length">
+              {{ lobby.teams2[i-1].name }}
+              <team-avatar right :team="{name: lobby.teams2[i-1].name, image: lobby.teams2[i-1].image}"/>
+            </v-chip>
+            <v-skeleton-loader v-else type="chip"/>
+          </v-col>
+        </v-row>
+      </v-col>
+    </v-row>
+  </div>
+</template>
+
+<script>
+  import TeamAvatar from "../team/TeamAvatar";
+
+  export default {
+    components: { TeamAvatar },
+    props: ["lobby"]
+  };
+</script>
+
+<style scoped>
+
+</style>
