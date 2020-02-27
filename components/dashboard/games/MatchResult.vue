44f2f2b53b1a261d96b8ced245677d7d42a8c664
Hamed <amzhamed@gmail.com>
2020-02-27 07:44:14 +0330

    Redesign match result

--- components/dashboard/games/MatchResult.vue
+++ components/dashboard/games/MatchResult.vue
@@ -1,46 +1,57 @@
 <template>
-  <v-list-item>
-    <v-list-item-content>
-      <div class="d-flex justify-center">
-        <div>
-          <v-chip>
-            <team-avatar left :team="{name:'team1'}"/>
-            team2
-          </v-chip>
-          <v-chip>
-            <team-avatar left :team="{name:'team1'}"/>
-            team2
-          </v-chip>
-        </div>
-        <div>
-          
-        </div>
-        <div>
-          <v-chip>
-            <team-avatar left :team="{name:'team1'}"/>
-            team2
-          </v-chip>
-          <v-chip>
-            <team-avatar left :team="{name:'team1'}"/>
-            team2
-          </v-chip>
-        </div>
-      </div>
-    </v-list-item-content>
-    <v-list-item-action>
-      <v-btn rounded depressed>
-        <v-icon left>mdi-download</v-icon>
-        {{ $t("dashboard.graphicLog") }}
-      </v-btn>
-    </v-list-item-action>
-  </v-list-item>
+  <div>
+    <v-row justify="center">
+      <v-col>
+        <v-row>
+          <v-col v-for="team in gameSides[0].game_teams" :key="team.team.name" cols="12" lg="6"
+                 class="d-flex justify-center px-0">
+            <v-badge
+              left
+              bordered
+              offset-y="26"
+              :color="gameSides[0].has_won ? 'success' : 'error'"
+              :content="team.score"
+              :value="team.score !== null"
+            >
+              <v-chip pill>
+                <team-avatar left :team="team.team"/>
+                {{ team.team.name }}
+              </v-chip>
+            </v-badge>
+          </v-col>
+        </v-row>
+      </v-col>
+      <v-col cols="1" class="d-flex align-center justify-center">
+        <v-icon>mdi-sword-cross</v-icon>
+      </v-col>
+      <v-col>
+        <v-row>
+          <v-col v-for="team in gameSides[1].game_teams" :key="team.team.name" cols="12" lg="6"
+                 class="d-flex justify-center px-0">
+            <v-badge
+              bordered
+              offset-y="26"
+              :color="gameSides[0].has_won ? 'success' : 'error'"
+              :content="team.score"
+              :value="team.score !== null"
+            >
+              <v-chip pill>
+                {{ team.team.name }}
+                <team-avatar right :team="team.team"/>
+              </v-chip>
+            </v-badge>
+          </v-col>
+        </v-row>
+      </v-col>
+    </v-row>
+  </div>
 </template>
 
 <script>
   import TeamAvatar from "../team/TeamAvatar";
 
   export default {
-    name: "MatchResult",
+    props: ["gameSides"],
     components: { TeamAvatar }
   };
 </script>
