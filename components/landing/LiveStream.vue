4826299a2cde29462e0fd7dd862562a8697403cf
Hamed <amzhamed@gmail.com>
2020-03-13 18:01:52 +0330

    Add live

---
+++ components/landing/LiveStream.vue
@@ -0,0 +1,40 @@
+<template>
+  <v-card class="overflow-hidden">
+    <v-tabs centered v-model="tab">
+      <v-tab v-for="tab in tabNames" :key="tab">
+        {{ $t(`dashboard.${tab}`) }}
+      </v-tab>
+    </v-tabs>
+    <v-divider/>
+    <v-tabs-items v-model="tab" class="mt-4">
+      <v-tab-item class="text-center">
+      <iframe src="https://player.twitch.tv/?channel=ai_challenge" frameborder="0" allowfullscreen="true" scrolling="no" height="500" width="820"></iframe><a href="https://www.twitch.tv/ai_challenge?tt_content=text_link&tt_medium=live_embed" style="padding:2px 0px 4px; display:block; width:345px; font-weight:normal; font-size:10px; text-decoration:underline;">Watch live video from AI_Challenge on www.twitch.tv</a>
+      </v-tab-item>
+      <v-tab-item>
+      <iframe src="https://challonge.com/aic_2020_final/module" width="100%" height="500" frameborder="0"
+              scrolling="auto" allowtransparency="true"
+              class="final_bracket"
+      ></iframe>
+      </v-tab-item>
+      <v-tab-item>
+
+      </v-tab-item>
+
+    </v-tabs-items>
+  </v-card>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        tabNames: ["closing", "scoreboard"],
+        tab: null
+      }
+    }
+  };
+</script>
+
+<style scoped>
+
+</style>
--- components/landing/Poster.vue
+++ components/landing/Poster.vue
@@ -1,75 +1,81 @@
 <template>
   <v-parallax height="650" src="/image/bg-hero.jpg">
-    <v-row
-      align="center"
-      justify="center"
-    >
-      <glow>
-        <v-col class="text-center" cols="12">
-          <div>
-            <span
-              class="display-1 display-xs-2 font-family-chopsic text-stroke-width-2 text-stroke-white transparent--text">sharif</span>
-            <span
-              class="display-2 display-xs-4 font-family-chopsic pl-3 text-stroke-width-2 text-stroke-primary transparent--text">AI</span>
-          </div>
-          <div
-            class="display-1 display-xs-2 font-family-chopsic text-stroke-width-2 text-stroke-white transparent--text">
-            Challenge
-          </div>
-          <div
-            class="display-2 display-xs-4 font-family-chopsic text-stroke-width-2 text-stroke-primary transparent--text">
-            2020
-          </div>
-        </v-col>
-      </glow>
+    <v-row>
+      <v-col>
+      <live-stream/>
+      </v-col>
     </v-row>
-    <v-row class="justify-center">
-      <div>
-        <v-btn v-if="!$auth.loggedIn" rounded x-large light color="primary" to="/signup" block>
-          <v-icon left>mdi-rocket</v-icon>
-          {{ $t("form.registerNow") }}
-        </v-btn>
-        <v-dialog v-model="dialog" width="900">
-          <template v-slot:activator="{ on }">
-            <v-btn
-              class="mt-5"
-              rounded
-              outlined
-              v-on="on"
-              block
-              x-large
-            >
-              <v-icon color="primary" left>mdi-play</v-icon>
-              {{ $t("home.watchTeaser") }}
-            </v-btn>
-          </template>
+<!--    <v-row-->
+<!--      align="center"-->
+<!--      justify="center"-->
+<!--    >-->
+<!--      <glow>-->
+<!--        <v-col class="text-center" cols="12">-->
+<!--          <div>-->
+<!--            <span-->
+<!--              class="display-1 display-xs-2 font-family-chopsic text-stroke-width-2 text-stroke-white transparent&#45;&#45;text">sharif</span>-->
+<!--            <span-->
+<!--              class="display-2 display-xs-4 font-family-chopsic pl-3 text-stroke-width-2 text-stroke-primary transparent&#45;&#45;text">AI</span>-->
+<!--          </div>-->
+<!--          <div-->
+<!--            class="display-1 display-xs-2 font-family-chopsic text-stroke-width-2 text-stroke-white transparent&#45;&#45;text">-->
+<!--            Challenge-->
+<!--          </div>-->
+<!--          <div-->
+<!--            class="display-2 display-xs-4 font-family-chopsic text-stroke-width-2 text-stroke-primary transparent&#45;&#45;text">-->
+<!--            2020-->
+<!--          </div>-->
+<!--        </v-col>-->
+<!--      </glow>-->
+<!--    </v-row>-->
+<!--    <v-row class="justify-center">-->
+<!--      <div>-->
+<!--        <v-btn v-if="!$auth.loggedIn" rounded x-large light color="primary" to="/signup" block>-->
+<!--          <v-icon left>mdi-rocket</v-icon>-->
+<!--          {{ $t("form.registerNow") }}-->
+<!--        </v-btn>-->
+<!--        <v-dialog v-model="dialog" width="900">-->
+<!--          <template v-slot:activator="{ on }">-->
+<!--            <v-btn-->
+<!--              class="mt-5"-->
+<!--              rounded-->
+<!--              outlined-->
+<!--              v-on="on"-->
+<!--              block-->
+<!--              x-large-->
+<!--            >-->
+<!--              <v-icon color="primary" left>mdi-play</v-icon>-->
+<!--              {{ $t("home.watchTeaser") }}-->
+<!--            </v-btn>-->
+<!--          </template>-->
 
-          <v-card>
-            <v-btn icon large class="p-absolute" style="z-index: 1" @click="dialog=false">
-              <v-icon>mdi-close</v-icon>
-            </v-btn>
-            <v-btn loading text color="accent" class="p-absolute" block style="top: 50%;"/>
-            <v-card-text class="pa-0" dir="ltr">
-              <v-responsive :aspect-ratio="16/9">
-                <keep-alive>
-                  <teaser v-if="dialog"/>
-                </keep-alive>
-              </v-responsive>
-            </v-card-text>
-          </v-card>
-        </v-dialog>
-      </div>
-    </v-row>
+<!--          <v-card>-->
+<!--            <v-btn icon large class="p-absolute" style="z-index: 1" @click="dialog=false">-->
+<!--              <v-icon>mdi-close</v-icon>-->
+<!--            </v-btn>-->
+<!--            <v-btn loading text color="accent" class="p-absolute" block style="top: 50%;"/>-->
+<!--            <v-card-text class="pa-0" dir="ltr">-->
+<!--              <v-responsive :aspect-ratio="16/9">-->
+<!--                <keep-alive>-->
+<!--                  <teaser v-if="dialog"/>-->
+<!--                </keep-alive>-->
+<!--              </v-responsive>-->
+<!--            </v-card-text>-->
+<!--          </v-card>-->
+<!--        </v-dialog>-->
+<!--      </div>-->
+<!--    </v-row>-->
   </v-parallax>
 </template>
 
 <script>
   import Glow from "../Glow";
   import Teaser from "./Teaser";
+  import LiveStream  from "./LiveStream";
 
   export default {
     name: "Poster",
-    components: { Teaser, Glow },
+    components: { Teaser, Glow, LiveStream },
     data() {
       return {
         dialog: false
